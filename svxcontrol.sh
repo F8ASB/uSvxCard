16f148d1275d37f0678713160eb6f0e28b167589
F8ASB <f8asb@hotmail.fr>
2020-12-07 07:40:29 +0100

    Add files via upload

--- svxcontrol.sh
+++ svxcontrol.sh
@@ -4,9 +4,9 @@ test1=$(grep -c "\#\* \* \* \* \* root pgrep svxlink" /etc/crontab)
   if [ $test1 = 0 ]
   then
    echo "Ajout du #"
-   sed -i '/root pgrep/ s/^/#/' /etc/crontab && service cron restart && pkill svxlink
+   sed -i '/root pgrep svxlink/ s/^/#/' /etc/crontab && service cron restart && pkill svxlink
  else 
 #  then
    echo "Enleve le #"
-   sed -i '/root pgrep/s/^#//' /etc/crontab && service cron restart && /etc/spotnik/restart
+   sed -i '/root pgrep svxlink/s/^#//' /etc/crontab && service cron restart && /etc/spotnik/restart
 fi
